d87d52e388fa01c0413e1c5d92994bd085d196fa
vimalraveendra <vimalraveendra@gmail.com>
2019-03-13 18:11:30 +0100

    Adding files to github

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card =({id,name, email}) =>{
+	return(
+       <div className ='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <img  alt='robots' src={`https://robohash.org/${id}?size=200x200` }/>
+        <h1 className='size'>{name}</h1>
+        <p>{email}</p>
+       </div>
+
+		)
+}
+
+export default Card;
