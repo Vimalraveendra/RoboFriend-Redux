f8c23d83c32b2d7167ea5ae6a32a3a67cc38509c
vimalraveendra <vimalraveendra@gmail.com>
2019-03-13 18:35:15 +0100

    Updates

---
+++ precache-manifest.d9e80d256c91162b445ec877fc6af4af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriend-Redux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d1a996fc9866c5947464",
+    "url": "/RoboFriend-Redux/static/js/runtime~main.d1a996fc.js"
+  },
+  {
+    "revision": "ea6aeaf7d426191ef95f",
+    "url": "/RoboFriend-Redux/static/js/main.ea6aeaf7.chunk.js"
+  },
+  {
+    "revision": "26e4b053769e1e29c5e4",
+    "url": "/RoboFriend-Redux/static/js/2.26e4b053.chunk.js"
+  },
+  {
+    "revision": "ea6aeaf7d426191ef95f",
+    "url": "/RoboFriend-Redux/static/css/main.25d0282f.chunk.css"
+  },
+  {
+    "revision": "26e4b053769e1e29c5e4",
+    "url": "/RoboFriend-Redux/static/css/2.052416f4.chunk.css"
+  },
+  {
+    "revision": "7cd90497e0b781f9cb794bba4aabaaed",
+    "url": "/RoboFriend-Redux/index.html"
+  }
+];
